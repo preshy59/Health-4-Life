990b133c4cf4ca8e5d9cea090f72388883abb630
Ebele Okemba <okemba50@gmail.com>
2023-03-16 21:12:33 +0000

    add styling to the home page

--- src/App.js
+++ src/App.js
@@ -11,27 +11,27 @@ import './App.css';
 function App() {
   return (
     <Router>
-    <div>
+      <div>
 
-      {/* rendering the header to all the pages with the project */}
-      <Header />
-      {/* Wrap Route elements in a Routes component */}
-      <Routes >
-        {/* Define routes using the Route component to render different page components at different paths */}
-        {/* Define a default route that will render the Home component */}
-        <Route path="/" element={<Home />} />
-        {/* definie a default route that will render the About Component */}
-        <Route path="/BMI" element={<BMI />} />
-      {/* definie a default route that will render the Exercise Component */}
-        <Route path="/Exercise" element={<Exercise />} />
-        {/* definie a default route that will render the Resource Component */}
-        <Route path="/Resources" element={<Resources />} />
-      </Routes>
-    </div>
-  </Router>
-);
+        {/* rendering the header to all the pages with the project */}
+        <Header />
+        {/* Wrap Route elements in a Routes component */}
+        <Routes >
+          {/* Define routes using the Route component to render different page components at different paths */}
+          {/* Define a default route that will render the Home component */}
+          <Route path="/" element={<Home />} />
+          {/* definie a default route that will render the About Component */}
+          <Route path="/BMI" element={<BMI />} />
+          {/* definie a default route that will render the Exercise Component */}
+          <Route path="/Exercise" element={<Exercise />} />
+          {/* definie a default route that will render the Resource Component */}
+          <Route path="/Resources" element={<Resources />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
-  
+
 
 
 export default App;
