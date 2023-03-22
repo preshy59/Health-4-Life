ca19d9ad45c8744b2f2d162181754570f51f8eaa
Sergio Utrera <sergiactive@gmail.com>
2023-03-22 15:56:08 +0000

    Added BMI Calculator

---
+++ src/Components/Pages/BMI/index.js
@@ -0,0 +1,165 @@
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+// import { QueryClient, QueryClientProvider } from "react-query";
+import URI from "urijs";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+// import Logo from '../../../../public/assets/images/_Health-4-Life-1.png';
+
+
+const fetchBMI = async (weight, height) => {
+	const url = URI("https://mega-fitness-calculator1.p.rapidapi.com/bmi")
+		.query({
+			weight,
+			height,
+		})
+		.toString();
+
+	const response = await fetch(url, {
+		method: "GET",
+		headers: {
+			'X-RapidAPI-Key': 'c327f58b16msh8eef6fb369749fep1feed3jsna36c18c50450',
+			'X-RapidAPI-Host': 'mega-fitness-calculator1.p.rapidapi.com'
+		},
+	});
+
+	return response.json();
+};
+
+export default function BMI() {
+	const [bodyParams, setBodyParams] = useState({
+		weight: 0,
+		height: 0,
+	});
+
+	let bmiQuery = useQuery(["bmi", bodyParams.weight, bodyParams.height], () =>
+		fetchBMI(bodyParams.weight, bodyParams.height)
+	);
+
+	const onWeightChange = (text) => {
+		setBodyParams({
+			...bodyParams,
+			weight: parseInt(text),
+		});
+	};
+
+	const onHeightChange = (text) => {
+		setBodyParams({
+			...bodyParams,
+			height: parseInt(text),
+		});
+	};
+
+	const BMI =
+		bodyParams.height && bodyParams.weight
+			? bmiQuery?.data?.info?.bmi
+			: "-----";
+
+	return (<>
+		<View style={styles.container}>
+			{/* <Image
+				style={styles.imageLogo}
+				source={require('./assets/images/_Health-4-Life-1.png')} /> */}
+			<Text style={{ fontSize: 30, fontWeight: "bold", color: "#77BB3F" }}>
+				BMI Calculator
+			</Text>
+			<TextInput
+				placeholder="Enter your weight in kg"
+				style={styles.input}
+				onChangeText={onWeightChange}
+			/>
+			<TextInput
+				placeholder="Enter your height in cm"
+				style={styles.input}
+				onChangeText={onHeightChange}
+			/>
+			<Text style={{ fontSize: 20, fontWeight: "bold", marginTop: "30" }}>
+				Your BMI is: {BMI}
+			</Text>
+
+			{BMI !== "-------" && (
+				<View>
+					{bmiQuery?.data?.info?.bmi < 18.5 && (
+						<>
+							<Text style={{ fontSize: 20, fontWeight: "bold" }}>
+								You are Underweight
+							</Text>
+						</>
+					)}
+
+					{bmiQuery?.data?.info?.bmi >= 18.5 &&
+						bmiQuery?.data?.info?.bmi < 24.9 && (
+							<>
+								<Text
+									style={{ fontSize: 20, fontWeight: "bold" }}
+								>
+									You are Normal
+								</Text>
+							</>
+						)}
+
+					{bmiQuery?.data?.info?.bmi >= 25 &&
+						bmiQuery?.data?.info?.bmi < 29.9 && (
+							<>
+								<Text
+									style={{ fontSize: 20, fontWeight: "bold" }}
+								>
+									You are Overweight
+								</Text>
+							</>
+						)}
+
+					{bmiQuery?.data?.info?.bmi >= 30 &&
+						bmiQuery?.data?.info?.bmi < 39.9 && (
+							<>
+								<Text
+									style={{ fontSize: 20, fontWeight: "bold" }}
+								>
+									You are Obese
+								</Text>
+							</>
+						)}
+
+					{bmiQuery?.data?.info?.bmi > 40 && (
+						<>
+							<Text style={{ fontSize: 20, fontWeight: "bold" }}>
+								You are Morbidly Obese
+							</Text>
+						</>
+					)}
+				</View>
+			)}
+			{/* <br />
+      <div className="2xl:container 2xl:mx-auto 2xl:px-0 py-3 px-10">
+        <Carousel />
+      </div>
+      <br /> */}
+
+		</View>
+	</>
+	);
+}
+
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		paddingTop: 300,
+		position: "relative",
+		top: "100px",
+	},
+	input: {
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		padding: 10,
+	},
+	imageLogo: {
+		height: "100px",
+		weight: "80px",
+		position: "relative",
+		top: "150px",
+	}
+});
+
