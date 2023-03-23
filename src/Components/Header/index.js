e0c2f35f6adf41b3254eafe231d17a769939555c
Ebele Okemba <okemba50@gmail.com>
2023-03-23 19:01:14 +0000

    replace the a tags with link from react dom

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Dialog } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Link } from 'react-router-dom';
 import "./style.css";
 
 const navigation = [
@@ -19,7 +20,7 @@ export default function Header() {
       <header className="absolute inset-x-0 top-0 z-50" id='header'>
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global" id='nav'>
           <div className="flex lg:flex-1">
-            <a href="/" className="-m-1.5 p-1.5" id='head'>
+            <Link to="/" className="-m-1.5 p-1.5" id='head'>
               <span className="sr-only">Your Company</span>
               <img
                 className="h-8 w-auto"
@@ -27,7 +28,7 @@ export default function Header() {
                 alt="site Logo"
                 id='logo'
               />
-            </a>
+            </Link>
           </div>
           <div className="flex lg:hidden">
             <button
@@ -41,10 +42,10 @@ export default function Header() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+              <Link key={item.name} to = {item.href} className="text-sm font-semibold leading-6 text-gray-900">
                 {item.name}
 
-              </a>
+              </Link>
             ))}
           </div>
         </nav>
