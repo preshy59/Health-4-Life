a281c360dbf74dca4fac1a1c0bcefee5e67ac25f
Ebele Okemba <okemba50@gmail.com>
2023-03-24 18:42:18 +0000

    replace the a tag with link from the react dom for responsiveness

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -56,14 +56,14 @@ export default function Header() {
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="/" className="-m-1.5 p-1.5">
+              <Link to="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <img
                   className="h-8 w-auto"
                   src="./assets/images/_Health-4-Life-1.png"
                   alt="site Logo"
                 />
-              </a>
+              </Link>
               {/* button to that display the link in a responsive mode */}
               <button
                 type="button"
@@ -78,13 +78,13 @@ export default function Header() {
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
                   {navigation.map((item) => (
-                    <a
+                    <Link
                       key={item.name}
-                      href={item.href}
+                      to={item.href}
                       className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
               </div>
